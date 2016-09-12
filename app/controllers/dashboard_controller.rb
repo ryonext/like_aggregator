f905beda5df02693d1c994050ae8293ebc0e16ef
ryonext <ryonext.s@gmail.com>
2016-09-12 22:48:49 +0900

    Add default page.

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,5 @@
+class DashboardController < ApplicationController
+  def index
+    render nothing: true, status: :ok
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
   resources :email
+  root "dashboard#index"
 end
