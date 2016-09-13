df618d448e61c339d525363ed989cb0568d21767
ryonext <ryonext.s@gmail.com>
2016-09-13 21:57:46 +0900

    Change method.

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,7 +1,7 @@
 require "gmail"
 
 class EmailController < ApplicationController
-  def index
+  def create
     twitter ||= Twitter::REST::Client.new do |config|
       config.consumer_key = ENV["CONSUMER_KEY"]
       config.consumer_secret = ENV["CONSUMER_SECRET"]
